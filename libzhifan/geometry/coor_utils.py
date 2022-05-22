5e2430a2b77c175a33a9c0483510ec15d468a18d
Zhifan Zhu <zhifan.zhu@bristol.ac.uk>
2022-05-22 22:24:42 +0100

    add geometry

---
+++ libzhifan/geometry/coor_utils.py
@@ -0,0 +1,265 @@
+""" Utility function for coordinate system. """
+import numpy as np
+
+""" Functions dealing with homogenous coordiate transforms. """
+
+
+def to_homo_xn(pts):
+    """ assume [x, n], output [x+1, n]"""
+    n = pts.shape[1]
+    return np.vstack((pts, np.ones([1, n])))
+
+
+def to_homo_nx(pts):
+    """ [n,x] -> [n,x+1] """
+    return to_homo_xn(pts.T).T
+
+
+def from_home_xn(pts):
+    """ [x+1, n] -> [x, n] """
+    return pts[:-1, :]
+
+
+def from_home_nx(pts):
+    """ [n, x+1] -> [n, x] """
+    return from_home_xn(pts.T).T
+
+
+def normalize_homo_xn(x_h):
+    return x_h / x_h[-1, :]
+
+
+def normalize_homo_nx(x_h):
+    return x_h / x_h[:, -1]
+
+
+def normalize_and_drop_homo_xn(x_h):
+    """ x_h: (c, n) -> (c-1, n) """
+    return x_h[:-1, :] / x_h[-1, :]
+
+
+def normalize_and_drop_homo_nx(x_h):
+    """ x_h: (n, c) -> (n, c-1) """
+    return normalize_and_drop_homo_xn(x_h.T).T
+
+
+def transform_nx3(transform_matrix, x):
+    """
+
+    Args:
+        transform_matrix: (4, 4)
+        x: (n, 3)
+
+    Returns: (n, 3)
+
+    """
+    return transform_3xn(transform_matrix, x.T).T
+
+
+def transform_3xn(transform_matrix, x):
+    """
+
+    Args:
+        transform_matrix: (4, 4)
+        x: (3, n)
+
+    Returns: (3, n)
+
+    """
+    x2 = transform_matrix @ to_homo_xn(x)
+    return from_home_xn(x2)
+
+
+def concat_rot_transl_3x4(rot, transl):
+    """
+    Args:
+        rot: (3, 3)
+        transl: (3, 1) or (3, )
+
+    Returns: (3, 4)
+
+    """
+    Rt = np.zeros([3, 4])
+    Rt[0:3, 0:3] = rot
+    Rt[0:3, -1] = transl.squeeze()
+    return Rt
+
+
+def concat_rot_transl_4x4(rot, transl):
+    """
+    Args:
+        rot: (3, 3)
+        transl: (3, 1) or (3, )
+
+    Returns: (4, 4)
+
+    """
+    Rt = np.zeros([4, 4])
+    Rt[0:3, 0:3] = rot
+    Rt[0:3, -1] = transl.squeeze()
+    Rt[-1, -1] = 1.0
+    return Rt
+
+
+def rotation_epfl(alpha, beta, gamma):
+    R = np.zeros([3, 3])
+    cos_a, sin_a = np.cos(alpha), np.sin(alpha)
+    cos_b, sin_b = np.cos(beta), np.sin(beta)
+    cos_g, sin_g = np.cos(gamma), np.sin(gamma)
+    R[0, 0] = cos_a * cos_g - cos_b * sin_a * sin_g
+    R[1, 0] = cos_g * sin_a + cos_a * cos_b * sin_g
+    R[2, 0] = sin_b * sin_g
+
+    R[0, 1] = -cos_b * cos_g * sin_a - cos_a * sin_g
+    R[1, 1] = cos_a * cos_b * cos_g - sin_a * sin_g
+    R[2, 1] = cos_g * sin_b
+
+    R[0, 2] = sin_a * sin_b
+    R[1, 2] = -cos_a * sin_b
+    R[2, 2] = cos_b
+
+    return R
+
+
+# def rotation_xyz_from_euler(x_rot, y_rot, z_rot):
+#     rz = np.float32([
+#         [np.cos(z_rot), np.sin(z_rot), 0],
+#         [-np.sin(z_rot), np.cos(z_rot), 0],
+#         [0, 0, 1]
+#     ])
+#     ry = np.float32([
+#         [np.cos(y_rot), 0, -np.sin(y_rot)],
+#         [0, 1, 0],
+#         [np.sin(y_rot), 0, np.cos(y_rot)],
+#     ])
+#     rx = np.float32([
+#         [1, 0, 0],
+#         [0, np.cos(x_rot), np.sin(x_rot)],
+#         [0, -np.sin(x_rot), np.cos(x_rot)],
+#     ])
+#     return rz @ ry @ rx
+
+
+""" Camera """
+
+
+def project_3d_2d(A, x3d):
+    """
+
+    Args:
+        A: (3, 3)
+        x3d: (3, n)
+
+    Returns: (2, n)
+
+    """
+    x2d_h = A @ x3d
+    return extract_pixel_homo_xn(x2d_h)
+
+
+def extract_pixel_homo_xn(x2d_h):
+    """ x2d_h: [3, n] -> [2, n] """
+    return normalize_and_drop_homo_xn(x2d_h)
+
+
+def extract_pixel_homo_nx(x2d_h):
+    """ x2d_h: [n, 3] -> [n, 2] """
+    return normalize_and_drop_homo_nx(x2d_h.T).T
+
+
+def world_to_camera_pipeline(x3d, M_intrinsic, M_offset=None, Tmw=None):
+    """
+    P_2d = M_offset @ divide_by_z() @ M_intrinsic @ P_3d
+        where P_3d = (x, y, z)
+
+    Args:
+        x3d: (n, 3)
+        M_offset: (3, 3)
+        M_intrinsic: (3, 3) or (3, 4)
+        Tmw: (4, 4)
+
+    Returns:
+
+    """
+    pts_h = to_homo_nx(x3d).T  # pts_h: (4, n)
+    if M_offset is None:
+        M_offset = np.identity(3)
+    if Tmw is None:
+        Tmw = np.identity(4)
+    if M_intrinsic.shape == (3, 3):
+        _M_intrinsic = np.zeros([3, 4])
+        _M_intrinsic[:3, :3] = M_intrinsic
+    elif M_intrinsic.shape == (3, 4):
+        _M_intrinsic = np.zeros([3, 4])
+        _M_intrinsic[:3, :] = M_intrinsic
+    else:
+        raise ValueError(f"Unexpected M_intrinsic shape: {M_intrinsic.shape}")
+
+    M_transformations = _M_intrinsic @ Tmw
+    pts_h = M_transformations @ pts_h
+    pts_h = normalize_homo_xn(pts_h)
+    pts_h = M_offset @ pts_h
+    pts_2d = from_home_xn(pts_h).transpose()  # (3,n) -> (n,2)
+
+    return pts_2d
+
+
+def world_to_camera_pipeline_test():
+    x3d = np.float32([
+        [10, 20, 10],
+        [0, -10, 2],
+        [-30, 0, 3],
+        [-5, -5, 1],
+    ])
+    fx = 4
+    fy = 2
+    M_intrinsic = np.float32([
+        [fx, 0, 0],
+        [0, fy, 0],
+        [0, 0,  1],
+    ])
+    x2d_true = np.float32([
+        [4, 4],
+        [0, -10],
+        [-40, 0],
+        [-20, -10]
+    ])
+    x2d_est = world_to_camera_pipeline(x3d, M_intrinsic)
+    np.testing.assert_almost_equal(x2d_true, x2d_est, verbose=True)
+
+
+def reverse_offset(x2d, M_offset):
+    """
+    A typical pipeline of projecting 3d points to 2d is:
+    P_2d = M_offset @ divide_by_z() @ M_intrinsic @ P_3d
+         = M_offset @ P_2d'
+
+    This function calculate P_2d' given P_2d and M_offset
+
+    Args:
+        x2d: (n, 2), above P_2d
+        M_offset: (3, 3)
+
+    Returns: (n, 2)
+
+    """
+    M_offset_inv = np.linalg.inv(M_offset)
+    x2d_h = to_homo_nx(x2d)
+    x2d_prim_h = x2d_h @ M_offset_inv.T
+    return from_home_nx(x2d_prim_h)
+
+
+""" Change of coordinate system. """
+
+def points_opencv_to_opengl(pts):
+    """
+    Args:
+        (n, 3)
+
+    Returns:
+        (n, 3)
+    """
+    pts = pts.copy()
+    pts[:, 1] = - pts[:, 1]
+    return pts
+
---
+++ libzhifan/geometry/example_meshes.py
@@ -0,0 +1,108 @@
+import numpy as np
+
+from .coor_utils import points_opencv_to_opengl
+
+
+def pivot_simplex(xlen=1, ylen=1.5, zlen=2):
+    """
+    Generate a simplex in 3D.
+
+    Returns:
+        vertices: (4, 3)
+        faces: (4, 3)
+    """
+    pts = np.float32([
+        [0, 0, 0],
+        [xlen, 0, 0],
+        [0, ylen, 0],
+        [0, 0, zlen]
+    ])
+    faces = np.int32([
+        [0, 2, 1],
+        [0, 3, 2],
+        [0, 1, 3],
+        [1, 2, 3]
+    ])
+    return pts, faces
+
+
+def canonical_cuboids(
+        x=0,
+        y=0,
+        z=0,
+        w=1,
+        h=1,
+        d=1,
+        convention='opengl'):
+    """
+    Generate a Cuboid/Cube.
+    See visualize_2d.py for definition of the corner locations.
+
+    Args:
+        convention: one of {'opencv', 'opengl'}
+
+    Returns:
+        vertices: (8, 3)
+        faces: (12, 3)
+    """
+    x, y, z = 0, 0, 0
+    # NOTE: following use OpenCV's coordinate convention,
+
+    # X axis point to the right
+    right = x + w / 2.0
+    _left = x - w / 2.0
+    # Y axis point downward
+    top = y - h / 2.0
+    bottom = y + h / 2.0
+    # Z axis point outward
+    front = z + d / 2.0
+    rear = z - d / 2.0
+
+    # List of 8 vertices of the box
+    pts = np.float32([
+        [_left, top, front],     # Front Top Left
+        [right, top, front],     # Front Top Right
+        [right, bottom, front],  # Front Bottom Right
+        [_left, bottom, front],  # Front Bottom Left
+        [_left, top, rear],      # Rear Top Left
+        [right, top, rear],      # Rear Top Right
+        [right, bottom, rear],   # Rear Bottom Right
+        [_left, bottom, rear],   # Rear Bottom Left
+    ])
+    faces = np.int32([
+        [1, 3, 2],
+        [0, 3, 1],
+        [2, 6, 5],
+        [5, 1, 2],
+        [6, 7, 4],
+        [4, 5, 6],
+        [7, 3, 0],
+        [0, 4, 7],
+        [0, 1, 5],
+        [5, 4, 0],
+        [7, 6, 2],
+        [2, 3, 7]
+    ])
+    if convention == 'opencv':
+        pass
+    elif convention == 'opengl':
+        pts = points_opencv_to_opengl(pts)
+
+    return pts, faces
+
+
+def canonical_pivots(
+    width=1,
+    height=1,
+    depth=1):
+    """ a.k.a XYZ-axes
+
+    Returns: (4, 3)
+    """
+    pts = np.float32([
+        [0, 0, 0],
+        [width, 0, 0],   # X-axis
+        [0, height, 0],  # Y-axis
+        [0, 0, depth],   # Z-axis
+    ])
+    return pts
